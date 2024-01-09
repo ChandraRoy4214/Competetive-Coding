13a4274b4c3f8665186ed6672427907768d865ed
ChandraRoy4214 <chandraroy4214@gamil.com>
2024-01-10 05:22:47 +0530

    few more problems solved

---
+++ LeetCode150/strings/Easy/romanToInteger.js
@@ -0,0 +1,33 @@
+let roman = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000,
+};
+
+function romanToInt(s) {
+  if (s.length < 1) return 'No roman roman';
+
+  let integer = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (i + 1 < s.length && roman[s[i]] < roman[s[i + 1]]) {
+      integer -= roman[s[i]];
+    } else {
+      integer += roman[s[i]];
+    }
+  }
+
+  return integer;
+}
+
+let s = 'LIX';
+s = 'LVIII';
+// s = 'MCMXCIV';
+
+const result = romanToInt(s);
+
+console.log('result: ', result);
---
+++ LeetCode150/strings/Medium/integerToRoman.js
@@ -0,0 +1,19 @@
+let roman = {
+  1: 'I',
+  5: 'V',
+  10: 'X',
+  50: 'L',
+  100: 'C',
+  500: 'D',
+  1000: 'M',
+};
+
+var intToRoman = function (num) {
+  // if (roman[num]) return roman[num];
+};
+
+let num = 3;
+
+const result = intToRoman(num);
+
+console.log('result: ', result);
